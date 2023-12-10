6c13f6e44d84b9443300cc25c3f3a12771e1aab7
me-mystic <Shrutikolla.2002@gmail.com>
2023-12-10 20:59:14 +0530

    .

--- src/assets/Components/focusPage/MusicPlayer.jsx
+++ src/assets/Components/focusPage/MusicPlayer.jsx
@@ -1,36 +1,101 @@
-import React from 'react'
+// toggle icons play pause
+import React, { useState, useRef, useEffect } from 'react'
+
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateBeforeNext from '@mui/icons-material/NavigateNext';
 
 import css from '../../css/player.module.css'
+
 const MusicPlayer = () => {
+
+    const audioRef = useRef('');
+    const progressBar = useRef('');
+    const volume = useRef('');
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        progressBar.current.max = audioRef.current.duration;
+        progressBar.current.value = audioRef.current.currentTime;
+        audioRef.current.volume = volume.current.value / 100;
+        volume.current.style.backgroundSize = `${volume.current.value}% 100%`
+    }, [])
+
     function handlePlay() {
-        console.log('play');
+        setIsPlaying(prev => {
+            if (prev == false) {
+                audioRef.current.play();
+                return true;
+            } else {
+                audioRef.current.pause();
+                return false;
+            }
+        })
+    }
+
+    // Progress Bar
+
+    function handleProgress(e) {
+        audioRef.current.currentTime = e.target.value;
+    }
+    function handleTime(){
+        console.log(progressBar.current);
+        progressBar.current.value = audioRef.current.currentTime;
+    }
+
+    // Volume Bar
+    function handleVol(e) {
+        audioRef.current.volume = e.target.value / 100;
+        volume.current.style.backgroundSize = `${e.target.value}% 100%`
+    }
+
+    // Back and Next
+
+    function handleBefore() {
+
+    }
+
+    function handleNext() {
+
     }
     return (
         <div>
-            <audio src="https://audio.jukehost.co.uk/MwWajxacPt9avPGIOkW1qzEof9QfYzVg" controls></audio>
+
+            <audio ref={audioRef} 
+            src="https://audio.jukehost.co.uk/MwWajxacPt9avPGIOkW1qzEof9QfYzVg" 
+            controls
+            onTimeUpdate={handleTime}
+            ></audio>
+
             <div className={css.player}>
-                <input type="range" value="0" className={css.progress} />
+
+                <input onInput={(e) => handleProgress(e)} ref={progressBar} type="range" className={css.progress} />
+
                 <div className={css.controls}>
-                    <button onClick={handlePlay}>
+                    <button onClick={handleBefore}>
                         <NavigateBeforeIcon
-                            className={css.backword}
+                            className={css.backward}
                         />
                     </button>
-                    <button onClick={handlePlay}>
-                        <PlayArrowIcon className={css.controlPlay} />
-                    </button>
-                    <button onClick={handlePlay}>
+
+                    {isPlaying ?
+                        <button onClick={handlePlay}>
+                            <PauseIcon className={css.controlPlay} />
+                        </button> :
+                        <button onClick={handlePlay}>
+                            <PlayArrowIcon className={css.controlPlay} />
+                        </button>
+                    }
+
+                    <button onClick={handleNext}>
                         <NavigateBeforeNext className={css.forward} />
                     </button>
-                    {/* <i className="fa-solid fa-backward" className=backward"></i> */}
-                    {/* <i className="fa-solid fa-play" id="controlPlay"></i> */}
-                    {/* <i className="fa-solid fa-forward" id="forward"></i> */}
-                    <input type="range" value="70" className={css.volume} />
+
+                    <input onInput={(e) => handleVol(e)} ref={volume} type="range" className={css.volume} />
                 </div>
             </div>
+
         </div>
     )
 }
