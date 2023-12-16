ab770b3e53f10f9040323d61baf84891ad4df486
me-mystic <Shrutikolla.2002@gmail.com>
2023-12-16 17:08:20 +0530

    .

--- src/assets/Components/focusPage/MusicPlayer.jsx
+++ src/assets/Components/focusPage/MusicPlayer.jsx
@@ -5,32 +5,35 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateBeforeNext from '@mui/icons-material/NavigateNext';
+import songsDb from './MusicDb.json';
 
 import css from '../../css/player.module.css'
 
 const MusicPlayer = () => {
 
+    const [currSong, setCurrSong] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const [lastBack, setLastBack] = useState('');
     const audioRef = useRef('');
     const progressBar = useRef('');
     const volume = useRef('');
-    const [isPlaying, setIsPlaying] = useState(false);
 
+    console.log(currSong);
+    
     useEffect(() => {
+        audioRef.current.src = songsDb[currSong].url;
+    }, [currSong])
+    function initialize() {
         progressBar.current.max = audioRef.current.duration;
         progressBar.current.value = audioRef.current.currentTime;
         audioRef.current.volume = volume.current.value / 100;
         volume.current.style.backgroundSize = `${volume.current.value}% 100%`
-    }, [])
+    }
 
     function handlePlay() {
         setIsPlaying(prev => {
-            if (prev == false) {
-                audioRef.current.play();
-                return true;
-            } else {
-                audioRef.current.pause();
-                return false;
-            }
+            prev ? audioRef.current.pause() : audioRef.current.play();
+            return !prev;
         })
     }
 
@@ -39,8 +42,7 @@ const MusicPlayer = () => {
     function handleProgress(e) {
         audioRef.current.currentTime = e.target.value;
     }
-    function handleTime(){
-        console.log(progressBar.current);
+    function handleTime() {
         progressBar.current.value = audioRef.current.currentTime;
     }
 
@@ -49,23 +51,76 @@ const MusicPlayer = () => {
         audioRef.current.volume = e.target.value / 100;
         volume.current.style.backgroundSize = `${e.target.value}% 100%`
     }
+    function volKeyStroke() {
+        volume.current.value = audioRef.current.volume * 100;
+        volume.current.style.backgroundSize = `${volume.current.value}% 100%`
+    }
 
     // Back and Next
 
     function handleBefore() {
-
+        const currTime = (new Date).getSeconds();
+        if (audioRef.current.currentTime < 10) {
+            if (currTime - lastBack < 2) {
+                changeSong(-1);
+            } else {
+                audioRef.current.currentTime = 0;
+            }
+        }
+        setLastBack(currTime);
+        audioRef.current.currentTime -= 10;
+        // console.log('10 sec back', audioRef.current.currentTime);
     }
 
     function handleNext() {
-
+        if (audioRef.current.currentTime > audioRef.current.duration - 10) {
+            console.log("next song");
+            changeSong(1);
+            return;
+        }
+        audioRef.current.currentTime += 10;
+        // console.log('10 sec fw', audioRef.current.currentTime);
     }
+
+    // prev next song
+
+    function changeSong(dir) {
+        const n = songsDb.length;
+        dir == 1 ? setCurrSong(curr => (curr + 1) % n) : setCurrSong(curr => (curr - 1 + n) % n)
+    }
+
+    // keyStrokes
+
+    // document.onkeydown = (e) => {
+    //     // console.log(e.code)
+    //     switch (e.code) {
+    //         case "ArrowLeft":
+    //             handleBefore();
+    //             break;
+    //         case "ArrowRight":
+    //             handleNext();
+    //             break;
+    //         case "ArrowUp":
+    //             audioRef.current.volume <= 0.8 ? audioRef.current.volume += 0.2 : audioRef.current.volume = 1;
+    //             volKeyStroke();
+    //             break;
+    //         case "ArrowDown":
+    //             audioRef.current.volume >= 0.2 ? audioRef.current.volume -= 0.2 : audioRef.current.volume = 0;
+    //             volKeyStroke();
+    //             break;
+    //         case "Space":
+    //             handlePlay();
+    //             break;
+    //     }
+    // }
     return (
         <div>
 
-            <audio ref={audioRef} 
-            src="https://audio.jukehost.co.uk/MwWajxacPt9avPGIOkW1qzEof9QfYzVg" 
-            controls
-            onTimeUpdate={handleTime}
+            <audio ref={audioRef}
+                controls autoPlay
+                onTimeUpdate={handleTime}
+                onLoadedMetadata={initialize}
+                onEnded={() => changeSong(1)}
             ></audio>
 
             <div className={css.player}>
